1b1a1a07f4e0d3902d24aae6a67c8c75cda2b755
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2021-06-15 19:46:18 -0700

    Se agrego dropdown menu al navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, {useState} from "react"
 import {Link} from "react-router-dom"
 
 import "./Navbar.css"
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import AppsIcon from '@material-ui/icons/Apps';
 
  const Navbar = () => {
 
@@ -20,9 +21,49 @@ import SearchIcon from '@material-ui/icons/Search';
             <input className="search" type="text" placeholder="Buscar..." />
         </div>
         
-        <MenuIcon className="iconoMenu"/>
+        <Navitem className="h1" icon={<MenuIcon className="iconoMenu" />}>
+        <Dropdown />
+        </Navitem>
+        {/* <MenuIcon className="iconoMenu">
+           
+            </MenuIcon> */}
      
     </div>
+    )
+}
+
+const Navitem = (props) =>{
+
+    const [open, setOpen] = useState(false)
+
+    return(
+        <li className="nav-item">
+            <a href="#" className="icon-button" onClick={(e) =>{ setOpen(!open); e.preventDefault()}}>
+                {props.icon}
+                </a>
+
+            {open && props.children} 
+        </li>
+    )
+}
+
+const Dropdown = () =>{
+    
+    const Dropdownitem = (props) =>{
+
+        return(
+            <a href="#" className="menu-item" onClick={(e)=> e.preventDefault()}>
+                {/* <span className="icon-button">{props.leftIcon}</span> */}
+                <p className="p"> {props.children} </p>
+                <span className="icon-right">{props.rightIcon}</span>
+            </a>
+        )
+    }
+    
+    return(
+        <div className="dropdown">
+            <Dropdownitem>My profile</Dropdownitem>
+        </div>
     )
 }
 
